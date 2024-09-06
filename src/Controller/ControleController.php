5befa9b34c25ff7e3cb4011536810392f5d19ce7
Tayou fom <wilfriedtayou6@gmail.com>
2024-09-06 14:14:44 +0200

    version 3.7.2

--- src/Controller/ControleController.php
+++ src/Controller/ControleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Controle;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,11 +22,28 @@ class ControleController extends AbstractController
     public function demandeList(ManagerRegistry $doctrine): Response
     {
         $demandeControle=$doctrine->getRepository(Controle::class)->findBy(['statut_demande' =>false]);
+        $controle=$doctrine->getRepository(Controle::class)->findBy(['statut_demande' =>true]);
 
         return $this->render('controle/demandeList.html.twig', [
             'controller_name' => 'ControleController',
             'demandeControles' => $demandeControle,
+            'demandesNonControlees' => $demandeControle,
+            'demandesControlees' => $controle,
             'active_page' => 'demandeList',
         ]);
+    }
+    #[Route('/controle/demande/date/{id}', name: 'app_controle.demandeDate')]
+    public function demandeDate($id,ManagerRegistry $doctrine,Request $request): Response
+    {   
+      
+        $date = new \DateTime($request->request->get("date_controle"));
+       
+        $demandeControle=$doctrine->getRepository(Controle::class)->find($id);
+        $demandeControle->setDateControle($date);
+        $entityManager=$doctrine->getManager();
+        $entityManager->persist($demandeControle);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_controle.demandeList');
     }
 }
